dff1fe2c1a918ede49270818665eac679ca72d2e
Robert Levelind <robert.levelind@tink.com>
2021-08-31 16:32:58 +0200

    database fix + shuffle button
    Adding categories and a shuffle button, also enabling a form of filtration for a later stage.

--- generator.js
+++ generator.js
@@ -3,22 +3,57 @@ fetch('places.json')
         return response.json();
     })
     .then(function (data) {
-        appendData(data);
+        appendData(data, null);
     })
     .catch(function (err) {
         console.log('error: ' + err);
     });
-function appendData(data) {
+
+function appendData(data, mask) {
     let mainContainer = document.getElementById("places");
     document.createElement("ol")
-    for (let i = 0; i < 10; i++) {
-        let first = Object.keys(data["prefix"][getRandomInt(data["prefix"].length)]);
-        let last = Object.keys(data["suffix"][getRandomInt(data["suffix"].length)]);
-        let li = document.createElement("li");
-        li.innerHTML = (first + last);
-        mainContainer.appendChild(li);
+    mainContainer.innerHTML=""
+    if (mask != null) {
+        console.log("Filter is: " + mask)
+        for (let i = 0; i < 10; i++) {
+            let filteredPrefix = (data["prefix"]).filter(function (i) {
+                return i["category"] === mask || i["category"] === "none"
+            })
+            let filteredSuffix = (data["suffix"]).filter(function (i) {
+                return i["category"] === mask || i["category"] === "none"
+            })
+            let first = filteredPrefix[getRandomInt(filteredPrefix.length)]["name"];
+            let last = filteredSuffix[getRandomInt(filteredSuffix.length)]["name"];
+            let li = document.createElement("li");
+            li.innerHTML = (first + last);
+            mainContainer.appendChild(li);
+        }
+
+    } else {
+        console.log("Filter is: " + mask)
+        for (let i = 0; i < 10; i++) {
+            let first = data["prefix"][getRandomInt(data["prefix"].length)]["name"];
+            let last = data["suffix"][getRandomInt(data["suffix"].length)]["name"];
+            let li = document.createElement("li");
+            li.innerHTML = (first + last);
+            mainContainer.appendChild(li);
+        }
     }
+
 }
 function getRandomInt(max){
     return Math.floor(Math.random() * max);
+}
+
+function filterPlaces(filter){
+    fetch('places.json')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            appendData(data, filter);
+        })
+        .catch(function (err) {
+            console.log('error: ' + err);
+        });
 }
